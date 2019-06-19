1f6725150d3dc7752ba44f2fc93b76bc2b85369f
gillianwenhold <gwenhold@mac.com>
2019-06-19 14:28:40 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  elsif int == 2
+    return true
+  else
+    array = (2...int-1).to_a
+    if array.any?{|n| int % n == 0}
+      return false
+    else
+      return true
+    end
+  end
+end
